d5886296ea936ceec5922bd1b53035c4d7275ee8
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2022-11-28 21:17:04 -0400

    Add cycles (work, break, work, break, ...)

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let timer = document.getElementById("timer");
-
+let cycle = document.getElementById("cycle");
 let startButton = document.getElementById("start");
 
 let running = false; // if timer is running
 
-let seconds = 0;
 let minutes = 0;
+let seconds = 0;
+
+let numOfCycles = 0; // pomodoro technique cycles
+
+let task = "Break"; // either "Work" or "Break"
 
 function updateTimer() {
     seconds++;
+
+    cycle.textContent = `${task} - Cycle: ${numOfCycles}`;
 
     // if a minute has passed, update minutes and seconds
     if (seconds == 60) {
@@ -23,16 +29,42 @@ function updateTimer() {
     // show new time
     timer.textContent = `${minutesString}:${secondsString}`
 
-    // if seconds are greater 
+    // when a minute passes
     if (seconds > 60) {
         seconds = 0;
         minutes++;
+    }
+
+    if (minutes == 25 && task == "Work") {
+        task = "Break";
+        seconds = 0;
+        minutes = 0;
+    }
+
+    // if the cycle is a multiple of four, use long break
+    if (numOfCycles % 4 == 0) {
+        // long break;
+        if (minutes == 15 && task == "Rest") {
+            task = "Work"
+            seconds = 0;
+            minutes = 0;
+            numOfCycles++;
+        }
+    } else {
+        // short break
+        if (minutes == 5 && task == "Rest") {
+            task = "Work"
+            seconds = 0;
+            minutes = 0;
+            numOfCycles++;
+        }
     }
 }
 
 startButton.addEventListener("click", () => {
     // make sure you cant have it going multiple times
     if (running == false) {
+        //numOfCycles = 1;
         setInterval(updateTimer, 1000);
         running = true;
     }
